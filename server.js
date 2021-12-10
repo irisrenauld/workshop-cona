1d78aa7aac443dba5ab0839d8bf43834bdf26042
irisrenauld <iris.renauld7@gmail.com>
2021-12-10 15:01:51 +0100

    server create and api

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middelware = jsonServer.defaults();
-const port = process.env.PORT || 3210;
+const port = process.env.PORT || 3000;
 
 server.use(middelware);
 server.use(router);
